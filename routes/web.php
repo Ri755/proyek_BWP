673a1f5e442aa94e53be6a53865e5e6eab7bbba4
owen550 <omoses611@gmail.com>
2024-12-28 00:04:18 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+#tes satu dua tiga
 Route::get('/', function () {
     return view('welcome');
 });
